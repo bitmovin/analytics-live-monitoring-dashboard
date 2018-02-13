21d8ff5d3954b598d91829456cd573e26c76d120
Clemens Helm <clemens.helm@gmail.com>
2018-02-13 17:26:59 +0100

    Use specific Bootstrap CSS url.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+          crossorigin="anonymous"
+        />
         <Authenticated>
           <Main>
           </Main>
