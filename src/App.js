30f07703d18b99057d0dfe75dcb467be969ea407
Peter Pfaffeneder <peter.pfaffeneder@bitmovin.com>
2018-05-15 14:56:00 +0200

    Reverted crossorigin

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-          crossOrigin="anonymous"
+          crossorigin="anonymous"
         />
         <Authenticated>
           <Main>
